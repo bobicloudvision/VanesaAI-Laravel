35535e436f42e7aa3d1217b071f81a84df5e275b
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-27 19:45:03 +0300

    update

--- python/dialog_nltk/train.py
+++ python/dialog_nltk/train.py
@@ -1,6 +1,6 @@
 import numpy as np
 import json
-
+import os
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -8,7 +8,8 @@ from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('intents.json', 'r', encoding="utf8") as f:
+chatbotFolder = os.path.dirname(os.path.abspath(__file__))
+with open(chatbotFolder + '/intents.json', 'r', encoding="utf8") as f:
     intents = json.load(f)
 
 all_words = []
