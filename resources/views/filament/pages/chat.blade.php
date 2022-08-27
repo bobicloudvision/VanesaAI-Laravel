e5424ff39b80ba2e1f1ade21ed220495968ee0a0
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-27 21:07:39 +0300

    update

--- resources/views/filament/pages/chat.blade.php
+++ resources/views/filament/pages/chat.blade.php
@@ -1,5 +1,26 @@
 <x-filament::page>
 
+
+
+    <div id="toast-notification" class="p-4 w-full text-gray-900 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-300" role="alert">
+
+        <div class="flex items-center">
+            <div class="inline-block relative shrink-0">
+                <img class="w-12 h-12 rounded-full" src="{{url('avatar.jpeg')}}" alt="">
+                <span class="inline-flex absolute right-0 bottom-0 justify-center items-center w-6 h-6 bg-blue-600 rounded-full">
+                <svg aria-hidden="true" class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Message icon</span>
+            </span>
+            </div>
+            <div class="ml-3 text-sm font-normal">
+                <div class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</div>
+                <div class="text-sm font-normal">commmented on your photo</div>
+                <span class="text-xs font-medium text-blue-600 dark:text-blue-500">a few seconds ago</span>
+            </div>
+        </div>
+    </div>
+
+
     <x-filament::card class="col-span-12">
 
     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
