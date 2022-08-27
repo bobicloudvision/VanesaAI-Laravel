cb26497eb7a1e3e4f6285c187b9ea89664bf747f
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-27 19:34:20 +0300

    update

--- app/Filament/Pages/TrainRobot.php
+++ app/Filament/Pages/TrainRobot.php
@@ -7,6 +7,14 @@ use Filament\Pages\Page;
 class TrainRobot extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
     protected static string $view = 'filament.pages.train-robot';
+
+    public $log = '';
+
+    public function startTrainingAction(): void
+    {
+        $this->log = 'Start training...';
+
+        
+    }
 }
--- resources/views/filament/pages/train-robot.blade.php
+++ resources/views/filament/pages/train-robot.blade.php
@@ -1,3 +1,18 @@
 <x-filament::page>
 
+    <x-filament::card class="col-span-12">
+        <x-filament::form wire:submit.prevent="startTrainingAction()">
+
+            Train new robot intents with Python AI tensorflow model!
+            <br />
+
+            <x-filament::button type="submit">
+               Start Training
+            </x-filament::button>
+            <br />
+            {{$log}}
+
+        </x-filament::form>
+    </x-filament::card>
+
 </x-filament::page>
