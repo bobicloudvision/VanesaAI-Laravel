cd1f20ac421faf2a4fa53ae06f75ee985dc8fd94
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-29 21:27:59 +0300

    update

--- python/dialog_nltk/train.py
+++ python/dialog_nltk/train.py
@@ -54,11 +54,11 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 
 # Hyper-parameters
-num_epochs = 4000
-batch_size = 10
-learning_rate = 0.01
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
 input_size = len(X_train[0])
-hidden_size = 10
+hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
