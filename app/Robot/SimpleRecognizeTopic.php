b26528c4e874e0aceeec792e5947b1c408d5c5b7
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-30 11:41:37 +0300

    Create SimpleRecognizeTopic.php

---
+++ app/Robot/SimpleRecognizeTopic.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Robot;
+
+class SimpleRecognizeTopic
+{
+    public $input = '';
+
+    public function setInput(string $text)
+    {
+        $this->input = $text;
+    }
+
+    public function getResponse()
+    {
+        
+    }
+}
