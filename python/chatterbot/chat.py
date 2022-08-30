2b177303c3d89f0ec6635a2320e61c5498a9391b
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-30 21:43:57 +0300

    update

---
+++ python/chatterbot/chat.py
@@ -0,0 +1,23 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot('Vanesa')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot_corpus/data/bulgarian/movies.yml")
+
+# Get a response to an input statement
+while True:
+    try:
+        user_input = input()
+
+        bot_response = chatbot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
