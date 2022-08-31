e8e3872c510ae81b455e11ce3f7654c1f1a25d60
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-31 13:24:06 +0300

    update

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -29,14 +29,57 @@ class ExampleTest extends DuskTestCase
             ");
             $browser->pause(3000);
             $browser->click('.js-cookie-policy-manage-dialog-accept-button');
-            $browser->pause(5000);
+            $browser->pause(1000);
 
-            $browser->type('#email', '');
-            $browser->type('#pass', '');
+            $browser->type('#email', env('FB_EMAIL'));
+            $browser->type('#pass', env('FB_PASSWORD'));
 
             $browser->click('#loginbutton');
-            $browser->pause(5000);
+            $browser->pause(2000);
 
+
+
+            $browser->pause(2000);
+            $threadsCount = $browser->script("
+            const nodes6 = document.getElementsByTagName(\"div\");
+            let threadsI = 0;
+            for (let i = 0; i < nodes6.length; i++) {
+                if (nodes6[i].getAttribute('data-testid') == 'mwthreadlist-item') {
+                     nodes6[i].classList.add('js-last-message-' + threadsI);
+                    threadsI++;
+                }
+            }
+            return threadsI;
+            ")[0];
+            $browser->pause(1000);
+
+            for ($i = 0; $i <= $threadsCount; $i++) {
+
+                $browser->click('.js-last-message-' . $i);
+                $browser->pause(3000);
+
+                // get last messages
+                $countLastMessages = $browser->script("
+              const nodesMes6 = document.getElementsByTagName(\"div\");
+               let userSendedMessages = [];
+                for (let i = 0; i < nodesMes6.length; i++) {
+                  if (nodesMes6[i].getAttribute('data-testid') == 'message-container') {
+                        userSendedMessages.push({
+                            msg: nodesMes6[i].innerText
+                        });
+                   }
+                }
+                return userSendedMessages;
+                ")[0];
+
+                dd($countLastMessages);
+
+            }
+            return;
+
+
+
+            // send message
             $browser->visit('https://www.messenger.com/t/');
             $browser->pause(3000);
             $browser->script("
