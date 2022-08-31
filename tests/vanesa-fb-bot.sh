ee48972a2de899654acc62b6e06178ff8589ccca
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-31 15:41:16 +0300

    Update vanesa-fb-bot.sh

--- tests/vanesa-fb-bot.sh
+++ tests/vanesa-fb-bot.sh
@@ -1,1 +1,2 @@
-C:\xamp-php81\php\php.exe C:\Users\bobkata\Games\VanesaAI-Laravel\artisan dusk --filter FacebookBot
+cd C:\xamp-php81\php\php.exe C:\Users\bobkata\Games\VanesaAI-Laravel
+C:\xamp-php81\php\php.exe artisan dusk --filter FacebookBot
